5415231a969eea279d4f8026650741df61637d2c
hello_chenchen <welcomechenchen@gmail.com>
2018-12-17 21:57:11 +0800

    antd not work, commit for back up

---
+++ src/ui/MainWindows.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import Button from 'antd';
+import 'antd/dist/antd.css';
+// import 'antd/lib/input/style/css';
+class MainWindows extends React.Component {
+  render() {
+    return <Button type="primary">Button</Button>;
+  }
+}
+
+export default MainWindows;
